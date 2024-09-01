f6e3c6da08141d15b0098842979aea9b3d8505f9
Emmanuel <emmanueloshin@yahoo.com>
2024-09-01 14:16:43 +0100

    Adding Sum of the numbers

--- Program.cs
+++ Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("Enter the first number");
 decimal num1 = decimal.Parse(Console.ReadLine());
 Console.WriteLine("Enter the second number");
 decimal num2 = decimal.Parse(Console.ReadLine());
+decimal sum = num1 + num2;
+Console.WriteLine("The sum of the two numbers is " + sum);
--- obj/Debug/net8.0/simplecalculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/simplecalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simplecalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cbdb2e8a017675ce4fc0af0f0ce3dfd653afe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("simplecalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simplecalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
