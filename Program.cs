e5cbdb2e8a017675ce4fc0af0f0ce3dfd653afe3
unknown <alukosam1985@gmail.com>
2024-09-01 14:01:21 +0100

    simple calculator app setup

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Transactions;
+
+Console.WriteLine("Welcome to simple calculator");
+Console.WriteLine("Enter the first number");
+decimal num1 = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+decimal num2 = decimal.Parse(Console.ReadLine());
